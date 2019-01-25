ca02ef40dab186218e4c384d5449efcc57be3476
pgruszczynski93 <pgruszczynski93@gmail.com>
2019-01-25 10:57:50 +0100

    Scrolling background added

---
+++ Assets/Scripts/SIBackgroundScroller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace SpaceInvaders
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class SIBackgroundScroller : MonoBehaviour, IMoveable
+    {
+
+        [SerializeField] private MeshRenderer _meshRenderer;
+        [SerializeField] private Vector2 _scrollOffset;
+
+        public void MoveObj()
+        {
+            if (_meshRenderer == null)
+            {
+                return;
+            }
+            _meshRenderer.material  .mainTextureOffset += _scrollOffset * Time.deltaTime;
+        }
+
+
+        /// <summary>
+        /// add to game started event
+        /// </summary>
+        private void Update()
+        {
+            MoveObj();
+        }
+    }
+}
+
--- Assets/Scripts/SIMovement.cs
+++ Assets/Scripts/SIMovement.cs
@@ -63,6 +63,5 @@ namespace SpaceInvaders
         {
             SIEventsHandler.OnPlayerMove?.Invoke();
         }
-
     }
 }
