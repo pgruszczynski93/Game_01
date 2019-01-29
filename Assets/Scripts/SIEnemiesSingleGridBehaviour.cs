daa8c693285d131307f3c2f74c8d2393e17f7657
pgruszczynski93 <pgruszczynski93@gmail.com>
2019-01-29 22:06:50 +0100

    Naive neighbour detection

--- Assets/Scripts/SIEnemiesSingleGridBehaviour.cs
+++ Assets/Scripts/SIEnemiesSingleGridBehaviour.cs
@@ -8,14 +8,20 @@ namespace SpaceInvaders
         [SerializeField] private GameObject[] _enemiesInGrid;
         [SerializeField] private SimpleTween2DInfo _enemyGridTweenInfo;
         [SerializeField] private GameObject[] _enemiesAbleToShoot;
+        [SerializeField] private LayerMask _collisionMask;
 
         private int _totalEnemies;
         private int _livingEnemies;
         private int _speedMultiplier;
         private int _enemiesInRow;
+
         private float _gridSize;
+        private float _lastRefreshTime;
+        private float _shotAbilityRefreshTime;
 
         private Transform _cachedTransform;
+        private Vector2 _raycastDirection;
+        private Vector2 _raycastOffset;
 
         public GameObject[] EnemiesInGrid
         {
@@ -61,6 +67,11 @@ namespace SpaceInvaders
             _enemiesAbleToShoot = new GameObject[_enemiesInRow];
             _enemyGridTweenInfo.startPos = SIEnemiesGridsMaster.Instance.GridInitialPosition;
             _enemyGridTweenInfo.endPos = SIEnemiesGridsMaster.Instance.GridScenePosition;
+            _gridSize = 0.5f;
+            _raycastOffset = new Vector2(0.0f, -_gridSize/2);
+            _raycastDirection = Vector2.down;
+            _shotAbilityRefreshTime = 0.2f;
+            _lastRefreshTime = 0.0f;
         }
 
         private void DecreaseEnemiesCount()
@@ -112,12 +123,26 @@ namespace SpaceInvaders
 
         private void GetEnemiesAbleToShoot()
         {
-            Vector2 verticalNeighbourPosition;
+            float timeSinceStartup = Time.time;
 
-            for (int i = 0; i < _totalEnemies; i++)
+            if (timeSinceStartup > _lastRefreshTime)
             {
+                Vector2 enemyRaycastPosition;
 
+                for (int i = 0; i < _totalEnemies; i++)
+                {
+                    enemyRaycastPosition = _enemiesInGrid[i].transform.position;
+                    enemyRaycastPosition += _raycastOffset;
+                    RaycastHit2D raycastHit2D = Physics2D.Raycast(enemyRaycastPosition, _raycastDirection, _gridSize, _collisionMask);
+
+                    //Debug.DrawRay(enemyRaycastPosition, _raycastDirection, Color.blue);
+                    if (raycastHit2D.collider != null)
+                    {
+                    }
+                }
+                _lastRefreshTime = timeSinceStartup + _shotAbilityRefreshTime;
             }
+
         }
     }
 }
