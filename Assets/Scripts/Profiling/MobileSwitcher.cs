a3560a489988598a6d720d1f57d34c0b90314a6f
pgruszczynski93 <pgruszczynski93@gmail.com>
2020-07-28 23:28:28 +0200

    URP Asset changes - less batches

---
+++ Assets/Scripts/Profiling/MobileSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+
+public class MobileSwitcher : MonoBehaviour
+{
+	
+    public SRPBatcherProfiler	m_srpProfiler;
+	
+    void OnGUI()
+    {
+        float offset = 256 + 50;
+        if (GUI.Button(new Rect(32, offset, 150, 150), "Toggle batcher"))
+        {
+            GraphicsSettings.useScriptableRenderPipelineBatching ^= true;
+        }
+        if (GUI.Button(new Rect(32 + 150 + 32, offset, 150, 150), "Toggle stats"))
+        {
+            m_srpProfiler.m_Enable ^= true;
+        }
+    }
+	
+}
