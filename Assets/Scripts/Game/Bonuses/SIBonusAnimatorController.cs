22c44ee95dd7a471278824e96610a9fa14e9b9b6
pgruszczynski93 <pgruszczynski93@gmail.com>
2021-03-23 00:08:40 +0100

    TO FIX: Bonus reparenting to manager

--- Assets/Scripts/Game/Bonuses/SIBonusAnimatorController.cs
+++ Assets/Scripts/Game/Bonuses/SIBonusAnimatorController.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 namespace SpaceInvaders {
     public class SIBonusAnimatorController : MonoBehaviour {
 
-        static readonly int DissolveAmountID = Shader.PropertyToID("DissolveAmount");
+        static readonly int DissolveAmountID = Shader.PropertyToID("_DissolveAmount");
         static readonly int BonusSpawnedID = Animator.StringToHash("BonusSpawned");
         static readonly int BonusCollectedID = Animator.StringToHash("BonusCollected");
 
+        [SerializeField] float _variantAnimationTime;
         [SerializeField] Animator _animator;
 
         Action onAnimationStarted;
         Action onAnimationFinished;
 
-        MaterialPropertyBlock _propertyBlock;
-        Coroutine _animationRoutine;
-
-        bool _isMeshAnimationRunning = false;
         float _dissolveAmountValue;
         MeshRenderer _bonusVariantRenderer;
+        MaterialPropertyBlock _propertyBlock;
+        Coroutine _animationRoutine;
+        
+        public void AnimateBonusVariantRenderer() {
+            DOTween.To(() => _dissolveAmountValue, newVal => _dissolveAmountValue = newVal, 0f, _variantAnimationTime) 
+                .OnUpdate(UpdateMaterialPropertyBlock);
+        }
 
         public void SetRendererToAnimate(MeshRenderer mRenderer) {
             if(_propertyBlock == null)
@@ -28,14 +32,6 @@ namespace SpaceInvaders {
             // make it better!!!
             _dissolveAmountValue = 1;
             _bonusVariantRenderer = mRenderer;
-        }
-        
-        public void AnimateBonusVariantRenderer() {
-            if (_isMeshAnimationRunning)
-                return;
-            _isMeshAnimationRunning = true;
-            DOTween.To(() => _dissolveAmountValue, newVal => _dissolveAmountValue = newVal, 0f, 1.5f) 
-                .OnUpdate(UpdateMaterialPropertyBlock);
         }
 
         void UpdateMaterialPropertyBlock() {
