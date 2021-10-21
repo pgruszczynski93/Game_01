3512c7683fc1fa982a4e9e46009d3d1441f4d9d9
pgruszczynski93 <pgruszczynski93@gmail.com>
2021-10-21 23:32:45 +0200

    Simple post process management added

--- Assets/Scripts/Game/Systems/SIPostProcessController.cs
+++ Assets/Scripts/Game/Systems/SIPostProcessController.cs
@@ -1,4 +1,6 @@
+using System;
 using Configs;
+using Sirenix.OdinInspector;
 using SpaceInvaders;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -14,9 +16,19 @@ namespace Project.Systems {
         Bloom _bloom;
         Vignette _vignette;
         
-        //to do: dodać scriptable objecty zawierajace setup postprocesów
-
         void Start() => Initialise();
+        void OnEnable() => SubscribeEvents();
+        void OnDisable() => UnsubscribeEvents();
+
+        void SubscribeEvents() {
+            SIBonusesEvents.OnBonusEnabled += HandleOnBonusEnabled;
+            SIBonusesEvents.OnBonusDisabled += HandleOnBonusDisabled;
+        }
+
+        void UnsubscribeEvents() {
+            SIBonusesEvents.OnBonusEnabled -= HandleOnBonusEnabled;
+            SIBonusesEvents.OnBonusDisabled -= HandleOnBonusDisabled;
+        }
 
         void Initialise() {
             _postProcessVolume.profile.TryGet(out _bloom);
@@ -25,6 +37,66 @@ namespace Project.Systems {
         
         public void SetTimeSpeedModifier(float modifier) {
             _effectChangeDuration = modifier;
+        }
+        
+        void HandleOnBonusEnabled(BonusSettings bonusSettings) {
+            switch(bonusSettings.bonusType) {
+                case BonusType.Health:
+                    break;
+                case BonusType.Projectile:
+                    break;
+                case BonusType.ShieldSystem:
+                    break;
+                case BonusType.LaserBeam:
+                    break;
+                case BonusType.RapidFire:
+                    break;
+                case BonusType.TimeSlowDown:
+                    SetSpeedModificationPostprocessConfig();
+                    break;
+            }
+        }
+        
+        void HandleOnBonusDisabled(BonusSettings bonusSettings) {
+            switch(bonusSettings.bonusType) {
+                case BonusType.Health:
+                    break;
+                case BonusType.Projectile:
+                    break;
+                case BonusType.ShieldSystem:
+                    break;
+                case BonusType.LaserBeam:
+                    break;
+                case BonusType.RapidFire:
+                    break;
+                case BonusType.TimeSlowDown:
+                    SetBasePostProcessConfig();
+                    break;
+            }
+        }
+        
+        [Button]
+        void SetBasePostProcessConfig() {
+            _bloom.threshold.value = _baseConfig.bloomThreshold;
+            _bloom.intensity.value = _baseConfig.bloomIntensity;
+            _bloom.scatter.value = _baseConfig.bloomScatter;
+            _bloom.tint.overrideState = false;
+            _bloom.tint.value = _baseConfig.bloomTintColor;
+
+            _vignette.intensity.value = _baseConfig.vignetteIntensity;
+            _vignette.smoothness.value = _baseConfig.vignetteSmoothness;
+        }
+
+        [Button]
+        void SetSpeedModificationPostprocessConfig() {
+            _bloom.threshold.value = _speedModificationConfig.bloomThreshold;
+            _bloom.intensity.value = _speedModificationConfig.bloomIntensity;
+            _bloom.scatter.value = _speedModificationConfig.bloomScatter;
+            _bloom.tint.overrideState = true;
+            _bloom.tint.value = _speedModificationConfig.bloomTintColor;
+
+            _vignette.intensity.value = _speedModificationConfig.vignetteIntensity;
+            _vignette.smoothness.value = _speedModificationConfig.vignetteSmoothness;
         }
     }
 }
